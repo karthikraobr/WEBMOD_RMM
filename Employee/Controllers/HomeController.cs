9e6c842abd25b9e7b9e866e2d9c39839103507d0
karthikraobr <karthik.0703@gmail.com>
2015-11-03 22:49:55 +0100

    Initial Commit

---
+++ Employee/Controllers/HomeController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Employee.Models;
+
+namespace Employee.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Employee()
+        {
+            return View();
+        }
+
+        public ActionResult Department()
+        {
+            return View();
+        }
+
+        public ActionResult EmployeeDetails(string employeeName)
+        {
+            EmployeeModel employee = null;
+            if (employeeName == "Tom")
+            {
+                 employee = new EmployeeModel
+                {
+                    Name = "Tom",
+                    Department = "Dev",
+                    Manager = "Dick",
+                    RoomNumber = "1"
+
+                };
+            }
+            else if (employeeName == "Dick")
+            {
+                 employee = new EmployeeModel
+                {
+                    Name = "Dick",
+                    Department = "DevOps",
+                    Manager = "Dick",
+                    RoomNumber = "1"
+
+                };
+            }
+            else
+            {
+                 employee = new EmployeeModel
+                {
+                    Name = "Harry",
+                    Department = "QA",
+                    Manager = "Dick",
+                    RoomNumber = "1"
+
+                };
+            }
+            return View(employee);
+        }
+
+        public ActionResult DepartmentDetails(string departmentName)
+        {
+            DepartmentModel department = null;
+            if (departmentName == "Dev")
+            {
+                department = new DepartmentModel
+                {
+                    Name = "Dev",
+                    Members = new List<string>
+                {
+                    "Tom"
+                }
+                };
+            }
+            else if (departmentName == "QA")
+            {
+                department = new DepartmentModel
+                {
+                    Name = "QA",
+                    Members = new List<string>
+                {
+                    "Harry"
+                }
+                };
+            }
+            else
+            {
+                department = new DepartmentModel
+                {
+                    Name = "DevOps",
+                    Members = new List<string>
+                {
+                    "Dick"
+                }
+                };
+            }
+            return View(department);
+        }
+    }
+}
---
+++ Employee/Models/EmployeeModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Employee.Models
+{
+    public class EmployeeModel
+    {
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public string RoomNumber { get; set; }
+        public string Manager { get; set; }
+    }
+
+    public class DepartmentModel
+    {
+        public string Name { get; set; }
+        public List<string> Members { get; set; }
+    }
+}
